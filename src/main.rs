18284e4824142b961d09801064cf631c8f57908c
NielsBongers <nielsbongers5@gmail.com>
2023-08-28 17:20:35 +0200

    Set rust-formatter on save

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,38 @@
-use rand::Rng; 
+use rand::Rng;
 
 mod particles;
 
-
-fn velocity_verlet(p1: &mut particles::Particle, p2: &mut particles::Particle, dt: f64) { 
-
-    if p1.has_deorbited() { 
-        println!("{}", p1.name); 
-        p1.deorbited = true; 
+fn velocity_verlet(p1: &mut particles::Particle, p2: &mut particles::Particle, dt: f64) {
+    if p1.has_deorbited() {
+        println!("{}", p1.name);
+        p1.deorbited = true;
     }
 
-    let p1_force_t = p1.forces(&p2); 
-    let p1_acceleration_t = p1.acceleration(p1_force_t); 
+    let p1_force_t = p1.forces(&p2);
+    let p1_acceleration_t = p1.acceleration(p1_force_t);
 
-    p1.update_velocity(p1_acceleration_t, dt); 
-    p1.update_position(dt); 
+    p1.update_velocity(p1_acceleration_t, dt);
+    p1.update_position(dt);
 
-    let p1_force_tdt= p1.gravity(&p2); 
-    let p1_acceleration_tdt = p1.acceleration(p1_force_tdt); 
+    let p1_force_tdt = p1.gravity(&p2);
+    let p1_acceleration_tdt = p1.acceleration(p1_force_tdt);
 
-    p1.update_velocity(p1_acceleration_tdt, dt); 
+    p1.update_velocity(p1_acceleration_tdt, dt);
 }
 
-fn main(){
+fn main() {
+    // Parameters
+    let number_particles: i64 = 50;
+    let maximum_velocity = 300.;
 
-    // Parameters 
-    let number_particles: i64 = 50; 
-    let maximum_velocity = 300.; 
+    let mut t = 0.0;
+    let t_end = 50000.0 * 95. * 60. + 500.;
+    // let t_end = 95.0 * 60.0;
+    let dt = 10e-0;
 
-    let mut t = 0.0; 
-    let t_end = 50000.0 * 95. * 60. + 500.; 
-    // let t_end = 95.0 * 60.0; 
-    let dt = 10e-0; 
-
-    // Initializing all the objects. 
-    let mut earth = particles::Particle { 
-        name: String::from("Earth"), 
+    // Initializing all the objects.
+    let mut earth = particles::Particle {
+        name: String::from("Earth"),
         x: 0.0,
         y: 0.0,
         z: 0.0,
@@ -43,50 +40,47 @@ fn main(){
         v_y: 0.0,
         v_z: 0.0,
         mass: 5.972e24,
-        deorbited: false
-    }; 
+        deorbited: false,
+    };
 
-    let mut object_vector: Vec<particles::Particle> = Vec::new(); 
+    let mut object_vector: Vec<particles::Particle> = Vec::new();
     let mut rng = rand::thread_rng();
 
-    for i in 0..number_particles { 
+    for i in 0..number_particles {
+        let default_particle: particles::Particle = particles::Particle {
+            name: format!("particle {}", i.to_string()),
 
-        let default_particle: particles::Particle = particles::Particle { 
-            name: format!("particle {}", i.to_string()), 
-
-            x: 6371000.0 + 413000.0, 
+            x: 6371000.0 + 413000.0,
             y: 0.0,
             z: 0.0,
             v_x: 0.0 + 2.0 * (rng.gen::<f64>() - 0.5) * maximum_velocity,
-            v_y: 7700.0 + 2.0 * (rng.gen::<f64>() - 0.5) * maximum_velocity, 
+            v_y: 7700.0 + 2.0 * (rng.gen::<f64>() - 0.5) * maximum_velocity,
             v_z: 0.0,
             mass: 1.0,
-            deorbited: false
-            }; 
+            deorbited: false,
+        };
 
-            default_particle.init(); 
-            object_vector.push(default_particle)
-    } 
+        default_particle.init();
+        object_vector.push(default_particle)
+    }
 
-    // Running the simulation. 
+    // Running the simulation.
     while t < t_end {
-
-        for mut particle in object_vector.iter_mut() { 
-            if particle.deorbited { 
-                continue; 
+        for mut particle in object_vector.iter_mut() {
+            if particle.deorbited {
+                continue;
             }
-            velocity_verlet(&mut particle, &mut earth, dt); 
-            particle.write(t); 
+            velocity_verlet(&mut particle, &mut earth, dt);
+            particle.write(t);
         }
 
-        t += dt; 
-    } 
+        t += dt;
+    }
 
-    // Visualizing using Python. 
+    // Visualizing using Python.
     // let mut cmd = std::process::Command::new("C:\\Program Files\\Python310\\python.exe");
-    // cmd.arg("D:\\Google Drive\\Rust\\velocity_verlet\\results\\post_processing.py"); 
-    // cmd.output().unwrap(); 
+    // cmd.arg("D:\\Google Drive\\Rust\\velocity_verlet\\results\\post_processing.py");
+    // cmd.output().unwrap();
 
-    println!("Done!"); 
-    
+    println!("Done!");
 }
